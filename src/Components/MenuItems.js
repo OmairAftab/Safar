e5a8f8382b2712b02e4e3b730da9f9958bcdf577
0mairAftab <omairch035@gmail.com>
2025-12-03 13:21:42 +0500

    updated hamburger menu

--- src/Components/MenuItems.js
+++ src/Components/MenuItems.js
@@ -1,40 +1,91 @@
+// export const MenuItems= [
+//     {
+//         title : "Home",
+//         url : "/",
+//         cName : "nav-links",
+//         icon : "material-symbols-outlined"
+
+//     },
+
+//      {
+//         title : "About",
+//         url : "/about",
+//         cName : "nav-links",
+//         icon : "fa-solid fa-circle-info"
+
+//     },
+
+//      {
+//         title : "Service",
+//         url : "/service",
+//         cName : "nav-links",
+//         icon : "fa-solid fa-briefcase"
+
+//     },
+
+//      {
+//         title : "Contact",
+//         url : "/contact",
+//         cName : "nav-links",
+//         icon : "fa-solid fa-address-book"
+
+//     },
+
+//      {
+//         title : "Sign Up",
+//         url : "/signup",
+//         cName : "nav-links-mobile"
+
+//     },
+// ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 export const MenuItems= [
     {
         title : "Home",
         url : "/",
         cName : "nav-links",
-        icon : "material-symbols-outlined"
-
+        icon : "fa-solid fa-house"
     },
-
-     {
+    {
         title : "About",
         url : "/about",
         cName : "nav-links",
         icon : "fa-solid fa-circle-info"
-
     },
-
-     {
+    {
         title : "Service",
         url : "/service",
         cName : "nav-links",
         icon : "fa-solid fa-briefcase"
-
     },
-
-     {
+    {
         title : "Contact",
         url : "/contact",
         cName : "nav-links",
         icon : "fa-solid fa-address-book"
-
     },
-
-     {
+    {
         title : "Sign Up",
         url : "/signup",
         cName : "nav-links-mobile"
-
     },
 ]
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,46 +1,96 @@
-import { useState } from "react";
+// import { useState } from "react";
+// import { Component } from "react";
+// import './NavbarStyles.css'
+// import { Link } from "react-router-dom";
+// import {MenuItems} from "./MenuItems";
+
+
+
+// class Navbar extends Component {
+  
+//     render(){
+//   return (
+//     <nav className='Navbar-items'>
+     
+//     <h1 className='navbar-logo'>Safar </h1>
+
+//     <div className="menu-icons">
+
+
+//     </div> 
+
+//     <ul className="nav-menu">
+//       {MenuItems.map((item,index)=>{
+//         return(
+//           <li  key={index} >
+
+//              <Link className={item.cName} to={item.url}>
+
+//                <i className="{item.icon}"></i>
+
+//         {item.title} </Link>
+//       </li>
+
+//         );
+//       })}
+    
+//     </ul>
+//      </nav>
+    
+//   )
+// }
+// }
+
+// export default Navbar
+
+
+
+
+
 import { Component } from "react";
 import './NavbarStyles.css'
 import { Link } from "react-router-dom";
-import {MenuItems} from "./MenuItems";
-
-
+import { MenuItems } from "./MenuItems";
 
 class Navbar extends Component {
-  
-    render(){
-  return (
-    <nav className='Navbar-items'>
-     
-    <h1 className='navbar-logo'>Safar </h1>
+  state = { clicked: false };
 
-    <div className="menu-icons">
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  }
 
-{/* <svg className="menu" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
-<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
-  */}
-    </div> 
+  closeMobileMenu = () => {
+    this.setState({ clicked: false });
+  }
 
-    <ul className="nav-menu">
-      {MenuItems.map((item,index)=>{
-        return(
-          <li  key={index} >
+  render() {
+    return (
+      <nav className='Navbar-items'>
+        <h1 className='navbar-logo'>Safar</h1>
 
-             <Link className={item.cName} to={item.url}>
+        <div className="menu-icons" onClick={this.handleClick}>
+          <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+        </div>
 
-               <i className="{item.icon}"></i>
-
-        {item.title} </Link>
-      </li>
-
-        );
-      })}
-    
-    </ul>
-     </nav>
-    
-  )
-}
+        <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+          {MenuItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <Link 
+                  className={item.cName} 
+                  to={item.url}
+                  onClick={this.closeMobileMenu}
+                >
+                  <i className={item.icon}></i>
+                  {item.title}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    )
+  }
 }
 
 export default Navbar
