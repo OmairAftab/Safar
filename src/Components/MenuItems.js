0ad7c9cd24eacb80fa8fcf0004e70b599bd89cde
0mairAftab <omairch035@gmail.com>
2025-12-03 13:57:53 +0500

    Removed  one unrequired thing

--- src/Components/MenuItems.js
+++ src/Components/MenuItems.js
@@ -83,9 +83,5 @@ export const MenuItems= [
         cName : "nav-links",
         icon : "fa-solid fa-address-book"
     },
-    {
-        title : "Sign Up",
-        url : "/signup",
-        cName : "nav-links-mobile"
-    },
+   
 ]
